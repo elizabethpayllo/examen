94ec28da4661d829101a3b6232ca1aaaf80b72d2
elizabethpayllo <elizabethpaylloespinoza@gmail.com>
2020-04-16 20:33:28 -0400

    examen de TIN

---
+++ js/main_libro.js
@@ -0,0 +1,21 @@
+$(document).ready(init);
+
+function init(){
+    $(mi_libro).booklet(
+        {
+            width:800,
+            heigh:600,
+
+            closed: true,
+            covers: true,
+            autoCenter: true,
+
+            pagePadding:0,
+            hoverWidth:100,
+
+        }
+    );
+    $("#bt_go").click(function(){
+        $("#mi_libro").booklet("gotopage",$("#in_go").val());
+    });
+}
